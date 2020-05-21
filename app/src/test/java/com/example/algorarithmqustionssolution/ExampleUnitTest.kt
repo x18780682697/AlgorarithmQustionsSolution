69ee35e0afbb837ad39e7199edea053c80bf03d8
x18780682697 <x18780682697@gmail.com>
2020-05-21 23:08:04 +0800

    initial commit

---
+++ app/src/test/java/com/example/algorarithmqustionssolution/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.algorarithmqustionssolution
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * 用于测试《剑指offer》上的算法解法
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
