6d93abef60a191842e02184d341a43f1b4e61199
x18780682697 <x18780682697@gmail.com>
2020-05-23 22:44:27 +0800

    04-重建二叉树(自解)

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -50,38 +50,22 @@ public class Solution {
      * @return 树的根结点
      */
     public static BinaryTreeNode construct(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
-
-        // 开始位置大于结束位置说明已经没有需要处理的元素了
-        if (ps > pe) {
+        if (ps > pe/*注意边界条件，ps与pe值相等*/){
             return null;
         }
-        // 取前序遍历的第一个数字，就是当前的根结点
-        int value = preorder[ps];
-        int index = is;
-        // 在中序遍历的数组中找根结点的位置
-        while (index <= ie && inorder[index] != value) {
-            index++;
+        int rootNodeValue = preorder[ps];
+        int inOrderIndex = is;
+        for(int index = is; index <= ie/*注意边界条件，index可取得ie值*/; index++){
+            if (inorder[index] == rootNodeValue){
+                inOrderIndex = index;
+            }
         }
-
-        // 如果在整个中序遍历的数组中没有找到，说明输入的参数是不合法的，抛出异常
-        if (index > ie) {
-            throw new RuntimeException("Invalid input");
-        }
-
-        // 创建当前的根结点，并且为结点赋值
         BinaryTreeNode node = new BinaryTreeNode();
-        node.value = value;
-
-        // 递归构建当前根结点的左子树，左子树的元素个数：index-is+1个
-        // 左子树对应的前序遍历的位置在[ps+1, ps+index-is]
-        // 左子树对应的中序遍历的位置在[is, index-1]
-        node.left = construct(preorder, ps + 1, ps + index - is, inorder, is, index - 1);
-        // 递归构建当前根结点的右子树，右子树的元素个数：ie-index个
-        // 右子树对应的前序遍历的位置在[ps+index-is+1, pe]
-        // 右子树对应的中序遍历的位置在[index+1, ie]
-        node.right = construct(preorder, ps + index - is + 1, pe, inorder, index + 1, ie);
-
-        // 返回创建的根结点
+        node.value = rootNodeValue;
+        node.left = construct(preorder, ps+1, ps+1+inOrderIndex-1-is,
+                inorder, is, inOrderIndex-1);
+        node.right = construct(preorder, ps+1+inOrderIndex-1-is+1, pe,
+                inorder, inOrderIndex+1, ie);
         return node;
     }
 
