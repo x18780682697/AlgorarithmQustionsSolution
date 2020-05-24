ffb25a44b3307e7d65995230e5e348adad8e94b4
x18780682697 <x18780682697@gmail.com>
2020-05-24 17:14:03 +0800

    23-二叉搜索树的后序遍历序列（自解）

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -37,42 +37,23 @@ public class Solution {
      * @return true：该数组是某二叉搜索树的后序遍历的结果。false：不是
      */
     public static boolean verifySequenceOfBST(int[] sequence, int start, int end) {
-
-        // 如果对应要处理的数据只有一个或者已经没有数据要处理（start>end）就返回true
-        if (start >= end) {
+        if (start >= end){
             return true;
         }
-
-        // 从左向右找第一个不小于根结点（sequence[end]）的元素的位置
         int index = start;
-        while (index < end - 1 && sequence[index] < sequence[end]) {
+        while (index < end-1 && sequence[index] < sequence[end]){
             index++;
         }
-
-        // 执行到此处[start, index-1]的元素都是小于根结点的（sequence[end]）
-        // [start, index-1]可以看作是根结点的左子树
-
-        // right用于记录第一个大于根结点的元素的位置
-
-        int right = index;
-
-        // 接下来要保证[index, end-1]的所有元素都是大于根根点的值
-        // 因为[index, end-1]是根结点的右子树
-        // 从第一个不小于根结点的元素开始，找第一个不大于根结点的元素
-        while (index < end - 1 && sequence[index] > sequence[end]) {
+        int rStart = index;
+        while (index < end-1 && sequence[index] > sequence[end]){
             index++;
         }
-        // 如果[index, end-1]中有小于等于根结点的元素，
-        // 不符合二叉搜索树的定义，返回false
-        if (index != end - 1) {
+        if (index == end-1){
+            return verifySequenceOfBST(sequence, start, rStart-1)
+                    && verifySequenceOfBST(sequence, rStart, end-1);
+        }else{
             return false;
         }
-
-        // 执行到此处说明直到目前为止，还是合法的
-        // [start, index-1]为根结点左子树的位置
-        // [index, end-1]为根结点右子树的位置
-        index = right;
-        return verifySequenceOfBST(sequence, start, index - 1) && verifySequenceOfBST(sequence, index, end - 1);
     }
 
     /**
