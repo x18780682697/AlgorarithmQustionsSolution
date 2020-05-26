5b66d31c11d037b2dd01ba467426d5f25c06c2b6
x18780682697 <x18780682697@gmail.com>
2020-05-26 08:27:44 +0800

    39-平衡二叉树(自解)

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -57,22 +57,19 @@ public class Solution {
     }
 
     public static boolean isBalancedHelper(BinaryTreeNode root, int[] depth) {
-        if (root == null) {
+        if (root == null){
             depth[0] = 0;
             return true;
         }
-
         int[] left = new int[1];
         int[] right = new int[1];
-
-        if (isBalancedHelper(root.left, left) && isBalancedHelper(root.right, right)) {
-            int diff = left[0] - right[0];
-            if (diff >= -1 && diff <= 1) {
-                depth[0] = 1 + (left[0] > right[0] ? left[0] : right[0]);
+        if (isBalancedHelper(root.left, left) && isBalancedHelper(root.right, right)){
+            int diffAbs = Math.abs(left[0] - right[0]);
+            if (diffAbs <= 1){
+                depth[0] = 1 + (Math.max(left[0], right[0]));
                 return true;
             }
         }
-
         return false;
     }
 
