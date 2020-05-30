83fd6ad72a862b9ac2f38167bc7ec0a9d3620c1e
x18780682697 <x18780682697@gmail.com>
2020-05-30 09:49:02 +0800

    61-序列化二叉树

--- app/src/test/java/com/example/algorarithmqustionssolution/Question61.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Question61.java
@@ -23,43 +23,6 @@ public class Question61 {
 //        BinaryTreeNode right;
 //    }
 
-    /**
-     * 题目：从上到下按层打印二叉树，同一层的结点按从左到右的顺序打印，每一层打印一行。
-     * @param root
-     */
-    public static void print(BinaryTreeNode root) {
-        if (root == null) {
-            return;
-        }
 
-        List<BinaryTreeNode> list = new LinkedList<>();
-        BinaryTreeNode node;
-        // 当前层的结点个数
-        int current = 1;
-        // 记录下一层的结点个数
-        int next = 0;
-        list.add(root);
-
-        while (list.size() > 0) {
-            node = list.remove(0);
-            current--;
-            System.out.printf("%-3d", node.val);
-
-            if (node.left != null) {
-                list.add(node.left);
-                next++;
-            }
-            if (node.right != null) {
-                list.add(node.right);
-                next++;
-            }
-
-            if (current ==0) {
-                System.out.println();
-                current = next;
-                next = 0;
-            }
-        }
-    }
 
 }
