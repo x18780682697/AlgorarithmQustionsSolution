7c4706863abb1d52d60c36a84a7118c224352fe9
x18780682697 <x18780682697@gmail.com>
2020-05-31 20:42:52 +0800

    14-链表中倒数第k个结点（自解）

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -20,39 +20,23 @@ public class Solution {
      * @return 倒数第k个结点
      */
     public static ListNode findKthToTail(ListNode head, int k) {
-
-        // 输入的链表不能为空，并且k大于0
-        if (k < 1 || head == null) {
+        if (head == null || k < 1){
             return null;
         }
-
-        // 指向头结点
-        ListNode pointer = head;
-
-        // 倒数第k个结点与倒数第一个结点相隔k-1个位置
-        // pointer先走k-1个位置
-        for (int i = 1; i < k; i++) {
-            // 说明还有结点
-            if (pointer.next != null) {
-                pointer = pointer.next;
-            }
-            // 已经没有节点了，但是i还没有到达k-1说明k太大，链表中没有那么多的元素
-            else {
-                // 返回结果
+        ListNode frontPointer = head;
+        for (int i=0; i<k-1; i++){
+            if (frontPointer.next == null){
                 return null;
+            }else{
+                frontPointer = frontPointer.next;
             }
-
         }
-
-        // pointer还没有走到链表的末尾，那么pointer和head一起走，
-        // 当pointer走到最后一个结点即，pointer.next=null时，head就是倒数第k个结点
-        while (pointer.next != null) {
-            head = head.next;
-            pointer = pointer.next;
+        ListNode backPointer = head;
+        while (frontPointer.next != null){
+            frontPointer = frontPointer.next;
+            backPointer = backPointer.next;
         }
-
-        // 返回结果
-        return head;
+        return backPointer;
     }
 
     /**
