dec6d68e28b48a0576a55ae26a9c82766e2f8c64
x18780682697 <x18780682697@gmail.com>
2020-05-31 18:34:33 +0800

    65-矩阵中的路径(BFS)(自解)

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -19,75 +19,43 @@ public class Solution {
      * @return 是否找到 true是，false否
      */
     public static boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        // 参数校验
-        if (matrix == null || matrix.length != rows * cols || str == null || str.length < 1) {
+        if (matrix == null || rows < 0 || cols < 0 || str == null){
             return false;
         }
-
-        // 变量初始化
-        boolean[] visited = new boolean[rows * cols];
-        for (int i = 0; i < visited.length; i++) {
-            visited[i] = false;
-        }
-
-        // 记录结果的数组，
-        int[] pathLength = {0};
-        // 以每一个点为起始进行搜索
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (hasPathCore(matrix, rows, cols, str, visited, i, j, pathLength)) {
+        for (int i=0; i<rows; i++){
+            for (int j=0; j<cols; j++){
+                int[] pathLen = new int[1];
+                boolean[] visitedArray = new boolean[rows * cols];
+                if (hasPathCore(matrix, rows, cols, str, i, j, pathLen, visitedArray)){
                     return true;
                 }
             }
         }
-
         return false;
     }
 
-    /**
-     * 回溯搜索算法
-     *
-     * @param matrix     输入矩阵
-     * @param rows       矩阵行数
-     * @param cols       矩阵列数
-     * @param str        要搜索的字符串
-     * @param visited    访问标记数组
-     * @param row        当前处理的行号
-     * @param col        当前处理的列号
-     * @param pathLength 已经处理的str中字符个数
-     * @return 是否找到 true是，false否
-     */
-    private static boolean hasPathCore(char[] matrix, int rows, int cols, char[] str, boolean[] visited,
-                                       int row, int col, int[] pathLength) {
-
-        if (pathLength[0] == str.length) {
+    private static boolean hasPathCore(char[] matrix, int rows, int cols, char[] str, int i, int j,
+                                       int[] pathLen, boolean[] visitedArray) {
+        if (pathLen[0] == str.length){
             return true;
         }
-
-        boolean hasPath = false;
-
-        // 判断位置是否合法
-        if (row >= 0 && row < rows
-                && col >= 0 && col < cols
-                && matrix[row * cols + col] == str[pathLength[0]]
-                && !visited[row * cols + col]) {
-
-            visited[row * cols + col] = true;
-            pathLength[0]++;
-
-            // 按左上右下进行回溯
-            hasPath = hasPathCore(matrix, rows, cols, str, visited, row, col - 1, pathLength)
-                    || hasPathCore(matrix, rows, cols, str, visited, row - 1, col, pathLength)
-                    || hasPathCore(matrix, rows, cols, str, visited, row, col + 1, pathLength)
-                    || hasPathCore(matrix, rows, cols, str, visited, row + 1, col, pathLength);
-
-            if (!hasPath) {
-                pathLength[0]--;
-                visited[row * cols + col] = false;
-            }
-
+        if (i < 0 || i >= rows || j < 0 || j>=cols){
+            return false;
         }
-
+        boolean hasPath = false;
+        int where = i*cols+j;
+        if (!visitedArray[where] && matrix[where] == str[pathLen[0]]){
+            pathLen[0]++;
+            visitedArray[where] = true;
+            hasPath = hasPathCore(matrix, rows, cols, str, i+1, j, pathLen, visitedArray)
+                    || hasPathCore(matrix, rows, cols, str, i-1, j, pathLen, visitedArray)
+                    || hasPathCore(matrix, rows, cols, str, i, j+1, pathLen, visitedArray)
+                    ||hasPathCore(matrix, rows, cols, str, i, j-1, pathLen, visitedArray);
+            if (!hasPath){
+                pathLen[0]--;
+                visitedArray[where] = false;
+            }
+        }
         return hasPath;
     }
 
