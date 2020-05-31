0523e0b29258d6e387c4b3e2524d772f25ab797b
x18780682697 <x18780682697@gmail.com>
2020-05-31 17:05:55 +0800

    65-矩阵中的路径(BFS)

--- app/src/test/java/com/example/algorarithmqustionssolution/Question65.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Question65.java
@@ -1,11 +1,15 @@
 package com.example.algorarithmqustionssolution;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import com.example.algorarithmqustionssolution.Solution.BinaryTreeNode;
 
+/**
+ * 矩阵中的路径(BFS)
+ *
+ * 网页链接：https://lrh1993.gitbooks.io/android_interview_guide/content/algorithm/For-offer/65.html
+ * 题号链接：https://zhuanlan.zhihu.com/p/56200260
+ */
 @SuppressWarnings("unused")
-public class Solution {
+public class Question65 {
 
     /**
      * 题目：请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。
@@ -91,66 +95,4 @@ public class Solution {
         return hasPath;
     }
 
-    /**
-     * 二叉树节点类
-     */
-    public static class BinaryTreeNode {
-        public int val;
-        public BinaryTreeNode left;
-        public BinaryTreeNode right;
-        public BinaryTreeNode parent;
-        public TreeNode origin;
-    }
-
-}
-
-
-/**
- * TreeNode和BinaryTreeNode之间的转换工具
- */
-class NodeTranslateUtil {
-    /**
-     * 转换为TreeNode
-     */
-    public static TreeNode toTreeNode(Solution.BinaryTreeNode node){
-        TreeNode treeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            treeNode = new TreeNode(node.val);
-            treeNode.left = toTreeNode(node.left);
-            treeNode.right = toTreeNode(node.right);
-            treeNode.next = toTreeNode(node.parent);
-        }
-        return treeNode;
-    }
-
-    /**
-     * 转换为BinaryTreeNode
-     */
-    public static Solution.BinaryTreeNode toBinaryTreeNode(TreeNode node){
-        Solution.BinaryTreeNode binaryTreeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            binaryTreeNode = new Solution.BinaryTreeNode();
-            binaryTreeNode.origin = node;
-            binaryTreeNode.val = node.val;
-            binaryTreeNode.left = toBinaryTreeNode(node.left);
-            binaryTreeNode.right = toBinaryTreeNode(node.right);
-            binaryTreeNode.parent = toBinaryTreeNode(node.next);
-        }
-        return binaryTreeNode;
-    }
-}
-
-/**
- * 牛客网二叉树定义
- */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode next = null;
-    TreeNode(int x) { val = x; }
 }
