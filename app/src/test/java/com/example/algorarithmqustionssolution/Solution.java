8f7b3e5930fac57951673647e741958521c6ae0d
x18780682697 <x18780682697@gmail.com>
2020-05-31 20:29:53 +0800

    14-链表中倒数第k个结点

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -6,36 +6,53 @@ import java.util.Stack;
 @SuppressWarnings("unused")
 public class Solution {
 
-    public static ArrayList<Integer> sResultList = new ArrayList<>();
-
-    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        sResultList.clear();
-        Solution.printListInverselyUsingRecursion(listNode);
-        return sResultList;
+    public ListNode FindKthToTail(ListNode head,int k) {
+        return findKthToTail(head, k);
     }
 
     /**
-     * 输入个链表的头结点，从尾到头反过来打印出每个结点的值
-     * 使用递归的方式进行
+     * 输入一个键表，输出该链表中倒数第k 个结点．为了符合大多数人的习惯，
+     * 本题从1开始计数，即链表的尾结点是倒数第1个结点．例如一个链表有6个结点，
+     * 从头结点开始它们的值依次是1、2、3、4、5 6。这个链表的倒数第3个结点是值为4的结点．
      *
-     * @param root 链表头结点
+     * @param head 链表的头结点
+     * @param k    倒数第k个结点
+     * @return 倒数第k个结点
      */
-    public static void printListInverselyUsingRecursion(ListNode root) {
-        if (root == null){
-            return;
+    public static ListNode findKthToTail(ListNode head, int k) {
+
+        // 输入的链表不能为空，并且k大于0
+        if (k < 1 || head == null) {
+            return null;
         }
-        Stack<ListNode> stack = new Stack<>();
-        ListNode currentNode = root;
-        // 从头到尾顺序入栈
-        while (currentNode != null){
-            stack.push(currentNode);
-            currentNode = currentNode.next;
+
+        // 指向头结点
+        ListNode pointer = head;
+
+        // 倒数第k个结点与倒数第一个结点相隔k-1个位置
+        // pointer先走k-1个位置
+        for (int i = 1; i < k; i++) {
+            // 说明还有结点
+            if (pointer.next != null) {
+                pointer = pointer.next;
+            }
+            // 已经没有节点了，但是i还没有到达k-1说明k太大，链表中没有那么多的元素
+            else {
+                // 返回结果
+                return null;
+            }
+
         }
-        // 从尾到头顺序出栈
-        while (!stack.isEmpty()){
-            currentNode = stack.pop();
-            sResultList.add(currentNode.val);
+
+        // pointer还没有走到链表的末尾，那么pointer和head一起走，
+        // 当pointer走到最后一个结点即，pointer.next=null时，head就是倒数第k个结点
+        while (pointer.next != null) {
+            head = head.next;
+            pointer = pointer.next;
         }
+
+        // 返回结果
+        return head;
     }
 
     /**
