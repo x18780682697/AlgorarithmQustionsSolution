4bf67a12f159b3545c0e9e446028f007c322db06
x18780682697 <x18780682697@gmail.com>
2020-05-31 19:11:53 +0800

    66-机器人的运动范围(DFS)（自解）

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -20,80 +20,36 @@ public class Solution {
      * @return 最多可走的方格
      */
     public static int movingCount(int threshold, int rows, int cols) {
-        // 参数校验
-        if (threshold < 0 || rows < 1 || cols < 1) {
+        if (rows < 0 || cols < 0){
             return 0;
         }
-
-        // 变量初始化
-        boolean[] visited = new boolean[rows * cols];
-        for (int i = 0; i < visited.length; i++) {
-            visited[i] = false;
-        }
-
-        return movingCountCore(threshold, rows, cols, 0, 0, visited);
+        boolean[] visitedArray = new boolean[rows*cols];
+        return movingCountCore(threshold, rows, cols, 0, 0, visitedArray);
     }
 
-    /**
-     * 递归回溯方法
-     *
-     * @param threshold 约束值
-     * @param rows      方格的行数
-     * @param cols      方格的列数
-     * @param row       当前处理的行号
-     * @param col       当前处理的列号
-     * @param visited   访问标记数组
-     * @return 最多可走的方格
-     */
-    private static int movingCountCore(int threshold, int rows, int cols,
-                                       int row, int col, boolean[] visited) {
-
-        int count = 0;
-
-        if (check(threshold, rows, cols, row, col, visited)) {
-            visited[row * cols + col] = true;
-            count = 1
-                    + movingCountCore(threshold, rows, cols, row - 1, col, visited)
-                    + movingCountCore(threshold, rows, cols, row, col - 1, visited)
-                    + movingCountCore(threshold, rows, cols, row + 1, col, visited)
-                    + movingCountCore(threshold, rows, cols, row, col + 1, visited);
+    private static int movingCountCore(int threshold, int rows, int cols, int row, int col, boolean[] visitedArray) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols){
+            return 0;
         }
-
+        int count = 0;
+        int where = row*cols+col;
+        if (!visitedArray[where] && (countDigitSum(row) + countDigitSum(col)) <= threshold){
+            visitedArray[where] = true;
+            count =  1 + movingCountCore(threshold, rows, cols, row+1, col, visitedArray)
+                    + movingCountCore(threshold, rows, cols, row-1, col, visitedArray)
+                    + movingCountCore(threshold, rows, cols, row, col+1, visitedArray)
+                    + movingCountCore(threshold, rows, cols, row, col-1, visitedArray);
+        }
         return count;
     }
 
-    /**
-     * 断机器人能否进入坐标为(row, col)的方格
-     *
-     * @param threshold 约束值
-     * @param rows      方格的行数
-     * @param cols      方格的列数
-     * @param row       当前处理的行号
-     * @param col       当前处理的列号
-     * @param visited   访问标记数组
-     * @return 是否可以进入，true是，false否
-     */
-    private static boolean check(int threshold, int rows, int cols,
-                                 int row, int col, boolean[] visited) {
-        return col >= 0 && col < cols
-                && row >= 0 && row < rows
-                && !visited[row * cols + col]
-                && (getDigitSum(col) + getDigitSum(row) <= threshold);
-    }
-
-    /**
-     * 一个数字的数位之和
-     *
-     * @param number 数字
-     * @return 数字的数位之和
-     */
-    private static int getDigitSum(int number) {
+    private static int countDigitSum(int num) {
         int result = 0;
-        while (number > 0) {
-            result += (number % 10);
-            number /= 10;
+        int tmpVal = num;
+        while (tmpVal > 0){
+            result += tmpVal % 10;
+            tmpVal = tmpVal / 10;
         }
-
         return result;
     }
 
