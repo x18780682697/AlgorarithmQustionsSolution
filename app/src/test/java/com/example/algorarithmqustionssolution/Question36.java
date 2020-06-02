64ab26c22d39629f7234d786fe2cbb20ac30a9a1
x18780682697 <x18780682697@gmail.com>
2020-06-02 08:13:25 +0800

    36-两个链表的第一个公共结点

---
+++ app/src/test/java/com/example/algorarithmqustionssolution/Question36.java
@@ -0,0 +1,56 @@
+package com.example.algorarithmqustionssolution;
+
+/**
+ * 两个链表的第一个公共结点
+ *
+ * 网页链接：https://lrh1993.gitbooks.io/android_interview_guide/content/algorithm/For-offer/36.html
+ * 题号链接：https://zhuanlan.zhihu.com/p/56200260
+ */
+@SuppressWarnings("unused")
+public class Question36 {
+
+    /**
+     * 找两个结点的第一个公共结点，如果没有找到返回null，方法比较好，考虑了两个链表中有null的情况
+     *
+     * @param head1 第一个链表
+     * @param head2 第二个链表
+     * @return 找到的公共结点，没有返回null
+     */
+    public static ListNode findFirstCommonNode(ListNode head1, ListNode head2) {
+        int length1 = getListLength(head1);
+        int length2 = getListLength(head2);
+
+        int diff = length1 - length2;
+        ListNode longListHead = head1;
+        ListNode shortListHead = head2;
+
+        if (diff < 0) {
+            longListHead = head2;
+            shortListHead = head1;
+            diff = length2 - length1;
+        }
+
+        for (int i = 0; i < diff; i++) {
+            longListHead = longListHead.next;
+        }
+
+        while (longListHead != null && shortListHead != null && longListHead != shortListHead) {
+            longListHead = longListHead.next;
+            shortListHead = shortListHead.next;
+        }
+
+        // 返回第一个相同的公共结点，如果没有返回null
+        return longListHead;
+    }
+
+    private static int getListLength(ListNode head) {
+        int result = 0;
+        while (head != null) {
+            result++;
+            head = head.next;
+        }
+
+        return result;
+    }
+
+}
--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -6,109 +6,52 @@ import java.util.Stack;
 @SuppressWarnings("unused")
 public class Solution {
 
-    public RandomListNode Clone(RandomListNode pHead)
-    {
-        return clone(pHead);
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        return findFirstCommonNode(pHead1, pHead2);
     }
 
     /**
-     * 实现函复制一个复杂链表。在复杂链表中，每个结点除了有一个next字段指向下一个结点外，
-     * 还有一个sibling字段指向链表中的任意结点或者NULL
+     * 找两个结点的第一个公共结点，如果没有找到返回null，方法比较好，考虑了两个链表中有null的情况
      *
-     * @param head 链表表头结点
-     * @return 复制结点的头结点
+     * @param head1 第一个链表
+     * @param head2 第二个链表
+     * @return 找到的公共结点，没有返回null
      */
-    public static RandomListNode clone(RandomListNode head) {
-        // 如果链表为空就直接返回空
-        if (head == null) {
-            return null;
+    public static ListNode findFirstCommonNode(ListNode head1, ListNode head2) {
+        int length1 = getListLength(head1);
+        int length2 = getListLength(head2);
+
+        int diff = length1 - length2;
+        ListNode longListHead = head1;
+        ListNode shortListHead = head2;
+
+        if (diff < 0) {
+            longListHead = head2;
+            shortListHead = head1;
+            diff = length2 - length1;
         }
 
-        // 先复制结点
-        cloneNodes(head);
-        // 再链接sibling字段
-        connectNodes(head);
-        // 将整个链表拆分，返回复制链表的头结点
-        return reconnectNodes(head);
+        for (int i = 0; i < diff; i++) {
+            longListHead = longListHead.next;
+        }
+
+        while (longListHead != null && shortListHead != null && longListHead != shortListHead) {
+            longListHead = longListHead.next;
+            shortListHead = shortListHead.next;
+        }
+
+        // 返回第一个相同的公共结点，如果没有返回null
+        return longListHead;
     }
 
-    /**
-     * 复制一个链表，并且将复制后的结点插入到被复制的结点后面，只链接复制结点的next字段
-     *
-     * @param head 待复制链表的头结点
-     */
-    public static void cloneNodes(RandomListNode head) {
-        // 如果链表不空，进行复制操作
+    private static int getListLength(ListNode head) {
+        int result = 0;
         while (head != null) {
-            // 创建一个新的结点
-            RandomListNode tmp = new RandomListNode(head.label);
-            // 将被复制结点的值传给复制结点
-            tmp.label = head.label;
-            // 复制结点的next指向下一个要被复制的结点
-            tmp.next = head.next;
-            // 被复制结点的next指向复制结点
-            head.next = tmp;
-            // 到些处就已经完成了一个结点的复制并且插入到被复制结点的后面
-            // heed指向下一个被复制结点的位置
-            head = tmp.next;
-        }
-    }
-
-    /**
-     * 设置复制结点的sibling字段
-     *
-     * @param head 链表的头结
-     */
-    public static void connectNodes(RandomListNode head) {
-        // 如链表不为空
-        while (head != null) {
-            // 当前处理的结点sibling字段不为空，则要设置其复制结点的sibling字段
-            if (head.random != null) {
-                // 复制结点的sibling指向被复制结点的sibling字段的下一个结点
-                // head.next：表求复制结点，
-                // head.sibling：表示被复制结点的sibling所指向的结点，
-                // 它的下一个结点就是它的复制结点
-                head.next.random = head.random.next;
-            }
-            // 指向下一个要处理的复制结点
-            head = head.next.next;
-        }
-    }
-
-    /**
-     * 刚复制结点和被复制结点拆开，还原被复制的链表，同时生成复制链表
-     *
-     * @param head 链表的头结点
-     * @return 复制链表的头结点
-     */
-    public static RandomListNode reconnectNodes(RandomListNode head) {
-
-        // 当链表为空就直接返回空
-        if (head == null) {
-            return null;
+            result++;
+            head = head.next;
         }
 
-        RandomListNode pointer = head;
-        RandomListNode oldTailNode = pointer;
-        pointer = pointer.next;
-        RandomListNode newHeadNode = pointer, newTailNode = pointer;
-        pointer = pointer.next;
-        boolean isSolveOldNode = true;
-        while (pointer != null){
-            if (isSolveOldNode){
-                oldTailNode.next = pointer;
-                oldTailNode = pointer;
-            }else{
-                newTailNode.next = pointer;
-                newTailNode = pointer;
-            }
-            isSolveOldNode = !isSolveOldNode;
-            pointer = pointer.next;
-        }
-        oldTailNode.next = null;
-        newTailNode.next = null;
-        // 返回复制链表的头结点
-        return newHeadNode;
+        return result;
     }
 
     /**
