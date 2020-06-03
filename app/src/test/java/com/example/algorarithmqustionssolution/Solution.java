000da0fee5494c92df123207a73b33dcfa382b70
OpenTeam <open-team@hikvision.com>
2020-06-03 20:46:03 +0800

    05-用两个栈实现队列(自解)

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -37,21 +37,14 @@ public class Solution {
 
         // 删除操作，在队列头部删除结点
         public T deleteHead() {
-
-            // 先判断弹出栈是否为空，如果为空就将插入栈的所有数据弹出栈，
-            // 并且将弹出的数据压入弹出栈中
-            if (stack2.isEmpty()) {
-                while (!stack1.isEmpty()) {
+            while (stack2.isEmpty()){
+                while (!stack1.isEmpty()){
                     stack2.add(stack1.pop());
                 }
             }
-
-            // 如果弹出栈中还没有数据就抛出异常
-            if (stack2.isEmpty()) {
-                throw new RuntimeException("No more element.");
+            if (stack2.isEmpty()){
+                throw new RuntimeException("no more");
             }
-
-            // 返回弹出栈的栈顶元素，对应的就是队首元素。
             return stack2.pop();
         }
     }
