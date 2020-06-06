d67b8ecc2cd8a040a90d82d13e27b3aa9aea6684
x18780682697 <x18780682697@gmail.com>
2020-06-06 09:11:58 +0800

    20-包含min函数的栈（自解）

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -39,36 +39,32 @@ public class Solution {
 
 
         public void push(int data) {
-            stack.push(data);  //直接往栈中添加数据
-
-            //在辅助栈中需要做判断
-            if (minStack.size() == 0 || data < minStack.peek()) {
+            stack.push(data);
+            if (minStack.size() == 0 || data < minStack.peek()){
                 minStack.push(data);
-            } else {
-                minStack.add(minStack.peek());   //【核心代码】peek方法返回的是栈顶的元素
+            }else{
+                minStack.push(minStack.peek());
             }
         }
 
         public int pop() throws Exception {
-            if (stack.size() == 0) {
-                throw new Exception("栈中为空");
+            if (stack.size() == 0){
+                throw new RuntimeException("no more");
             }
-
-            int data = stack.pop();
-            minStack.pop();  //核心代码
-            return data;
+            minStack.pop();
+            return stack.pop();
         }
 
         public int min() throws Exception {
-            if (minStack.size() == 0) {
-                throw new Exception("栈中空了");
+            if (stack.size() == 0){
+                throw new RuntimeException("no more");
             }
             return minStack.peek();
         }
 
         public int peek() throws Exception {
-            if (stack.size() == 0) {
-                throw new Exception("栈中空了");
+            if (stack.size() == 0){
+                throw new RuntimeException("no more");
             }
             return stack.peek();
         }
