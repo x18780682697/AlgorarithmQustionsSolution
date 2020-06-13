f62bf2bf2089769ab20743a089d8ee807b23a4d2
x18780682697 <x18780682697@gmail.com>
2020-06-13 22:15:15 +0800

    06-旋转数组的最小数字（二分查找）

--- app/src/test/java/com/example/algorarithmqustionssolution/Question06.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Question06.java
@@ -1,21 +1,13 @@
 package com.example.algorarithmqustionssolution;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
-
+/**
+ * 06-旋转数组的最小数字（二分查找）
+ *
+ * 网页链接：https://lrh1993.gitbooks.io/android_interview_guide/content/algorithm/For-offer/6.html
+ * 题号链接：https://zhuanlan.zhihu.com/p/56200260
+ */
 @SuppressWarnings("unused")
-public class Solution {
-
-    public int minNumberInRotateArray(int [] array) {
-        return min(array);
-    }
+public class Question06 {
 
     /**
      * @param numbers 旋转数组
@@ -82,66 +74,4 @@ public class Solution {
         return result;
     }
 
-    /**
-     * 二叉树节点类
-     */
-    public static class BinaryTreeNode {
-        public int val;
-        public BinaryTreeNode left;
-        public BinaryTreeNode right;
-        public BinaryTreeNode parent;
-        public TreeNode origin;
-    }
-
-}
-
-
-/**
- * TreeNode和BinaryTreeNode之间的转换工具
- */
-class NodeTranslateUtil {
-    /**
-     * 转换为TreeNode
-     */
-    public static TreeNode toTreeNode(Solution.BinaryTreeNode node){
-        TreeNode treeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            treeNode = new TreeNode(node.val);
-            treeNode.left = toTreeNode(node.left);
-            treeNode.right = toTreeNode(node.right);
-            treeNode.next = toTreeNode(node.parent);
-        }
-        return treeNode;
-    }
-
-    /**
-     * 转换为BinaryTreeNode
-     */
-    public static Solution.BinaryTreeNode toBinaryTreeNode(TreeNode node){
-        Solution.BinaryTreeNode binaryTreeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            binaryTreeNode = new Solution.BinaryTreeNode();
-            binaryTreeNode.origin = node;
-            binaryTreeNode.val = node.val;
-            binaryTreeNode.left = toBinaryTreeNode(node.left);
-            binaryTreeNode.right = toBinaryTreeNode(node.right);
-            binaryTreeNode.parent = toBinaryTreeNode(node.next);
-        }
-        return binaryTreeNode;
-    }
-}
-
-/**
- * 牛客网二叉树定义
- */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode next = null;
-    TreeNode(int x) { val = x; }
 }
