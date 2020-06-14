9a1cf5ec1dbf87ad7f56e2fdcf911a739d37e7a7
x18780682697 <x18780682697@gmail.com>
2020-06-14 18:01:06 +0800

    52-正则表达式匹配

--- app/src/test/java/com/example/algorarithmqustionssolution/Question52.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Question52.java
@@ -1,15 +1,15 @@
 package com.example.algorarithmqustionssolution;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
+/**
+ * 52-正则表达式匹配
+ * （网页链接所指的题干有无，以牛客网的为准）
+ * https://www.nowcoder.com/practice/45327ae22b7b413ea21df13ee7d6429c?tpId=13&&tqId=11205&rp=1&ru=/activity/oj&qru=/ta/coding-interviews/question-ranking
+ *
+ * 网页链接：https://lrh1993.gitbooks.io/android_interview_guide/content/algorithm/For-offer/51.html
+ * 题号链接：https://zhuanlan.zhihu.com/p/56200260
+ */
 @SuppressWarnings("unused")
-public class Solution {
-
-    public boolean match(char[] str, char[] pattern)
-    {
-        return match(new String(str), new String(pattern));
-    }
+public class Question52 {
 
     /**
      * 题目：请实现一个函数用来匹配包含‘.’和‘*’的正则表达式。模式中的字符'.'表示任意一个字符，
@@ -79,66 +79,4 @@ public class Solution {
         return false;
     }
 
-    /**
-     * 二叉树节点类
-     */
-    public static class BinaryTreeNode {
-        public int val;
-        public BinaryTreeNode left;
-        public BinaryTreeNode right;
-        public BinaryTreeNode parent;
-        public TreeNode origin;
-    }
-
-}
-
-
-/**
- * TreeNode和BinaryTreeNode之间的转换工具
- */
-class NodeTranslateUtil {
-    /**
-     * 转换为TreeNode
-     */
-    public static TreeNode toTreeNode(Solution.BinaryTreeNode node){
-        TreeNode treeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            treeNode = new TreeNode(node.val);
-            treeNode.left = toTreeNode(node.left);
-            treeNode.right = toTreeNode(node.right);
-            treeNode.next = toTreeNode(node.parent);
-        }
-        return treeNode;
-    }
-
-    /**
-     * 转换为BinaryTreeNode
-     */
-    public static Solution.BinaryTreeNode toBinaryTreeNode(TreeNode node){
-        Solution.BinaryTreeNode binaryTreeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            binaryTreeNode = new Solution.BinaryTreeNode();
-            binaryTreeNode.origin = node;
-            binaryTreeNode.val = node.val;
-            binaryTreeNode.left = toBinaryTreeNode(node.left);
-            binaryTreeNode.right = toBinaryTreeNode(node.right);
-            binaryTreeNode.parent = toBinaryTreeNode(node.next);
-        }
-        return binaryTreeNode;
-    }
-}
-
-/**
- * 牛客网二叉树定义
- */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode next = null;
-    TreeNode(int x) { val = x; }
 }
