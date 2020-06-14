9c2895cb47b14527983581af70a45739dd4264a1
x18780682697 <x18780682697@gmail.com>
2020-06-14 08:35:31 +0800

    37-数字在排序数组中出现的次数（二分查找）

--- app/src/test/java/com/example/algorarithmqustionssolution/Question37.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Question37.java
@@ -1,24 +1,13 @@
 package com.example.algorarithmqustionssolution;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
-
+/**
+ * 37-数字在排序数组中出现的次数（二分查找）
+ *
+ * 网页链接：https://lrh1993.gitbooks.io/android_interview_guide/content/algorithm/For-offer/35.html
+ * 题号链接：https://zhuanlan.zhihu.com/p/56200260
+ */
 @SuppressWarnings("unused")
-public class Solution {
-
-    public int GetNumberOfK(int [] array , int k) {
-        if (array == null || array.length == 0){
-            return 0;
-        }
-        return getNumberOfK(array, k);
-    }
+public class Question37 {
 
     /**
      * 找排序数组中k第一次出现的位置
@@ -104,66 +93,4 @@ public class Solution {
         return number;
     }
 
-    /**
-     * 二叉树节点类
-     */
-    public static class BinaryTreeNode {
-        public int val;
-        public BinaryTreeNode left;
-        public BinaryTreeNode right;
-        public BinaryTreeNode parent;
-        public TreeNode origin;
-    }
-
-}
-
-
-/**
- * TreeNode和BinaryTreeNode之间的转换工具
- */
-class NodeTranslateUtil {
-    /**
-     * 转换为TreeNode
-     */
-    public static TreeNode toTreeNode(Solution.BinaryTreeNode node){
-        TreeNode treeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            treeNode = new TreeNode(node.val);
-            treeNode.left = toTreeNode(node.left);
-            treeNode.right = toTreeNode(node.right);
-            treeNode.next = toTreeNode(node.parent);
-        }
-        return treeNode;
-    }
-
-    /**
-     * 转换为BinaryTreeNode
-     */
-    public static Solution.BinaryTreeNode toBinaryTreeNode(TreeNode node){
-        Solution.BinaryTreeNode binaryTreeNode = null;
-        if (node == null){
-            return null;
-        }else{
-            binaryTreeNode = new Solution.BinaryTreeNode();
-            binaryTreeNode.origin = node;
-            binaryTreeNode.val = node.val;
-            binaryTreeNode.left = toBinaryTreeNode(node.left);
-            binaryTreeNode.right = toBinaryTreeNode(node.right);
-            binaryTreeNode.parent = toBinaryTreeNode(node.next);
-        }
-        return binaryTreeNode;
-    }
-}
-
-/**
- * 牛客网二叉树定义
- */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode next = null;
-    TreeNode(int x) { val = x; }
 }
