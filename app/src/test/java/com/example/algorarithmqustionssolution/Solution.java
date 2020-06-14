ed8d14defbda544cc53caeee233e4cfe4611f118
x18780682697 <x18780682697@gmail.com>
2020-06-14 23:05:39 +0800

    11-二进制中1的个数（自解）

--- app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
+++ app/src/test/java/com/example/algorarithmqustionssolution/Solution.java
@@ -18,38 +18,16 @@ public class Solution {
      * @return 数字中二进制表表的1的数目
      */
     public static int numberOfOne(int n) {
-        // 记录数字中1的位数
-        int result = 0;
-
-        // JAVA语言规范中，int整形占四个字节，总计32位
-        // 对每一个位置与1进行求与操作，再累加就可以求出当前数字的表示是多少位1
-        for (int i = 0; i < 32; i++) {
-            result += (n & 1);
-            n >>>= 1;
+        int cnt = 0;
+        int leftMoveLen = 0;
+        while (leftMoveLen < 32){
+            int tag = 1<<leftMoveLen;
+            if ((tag&n) == tag){
+                cnt++;
+            }
+            leftMoveLen++;
         }
-
-        // 返回求得的结果
-        return result;
-    }
-
-    /**
-     * @param n 待的数字
-     * @return 数字中二进制表表的1的数目
-     */
-    public static int numberOfOne2(int n) {
-        // 记录数字中1的位数
-        int result = 0;
-
-        // 数字的二进制表示中有多少个1就进行多少次操作
-        while (n != 0) {
-            result++;
-            // 从最右边的1开始，每一次操作都使n的最右的一个1变成了0，
-            // 即使是符号位也会进行操作。
-            n = (n - 1) & n;
-        }
-
-        // 返回求得的结果
-        return result;
+        return cnt;
     }
 
     /**
